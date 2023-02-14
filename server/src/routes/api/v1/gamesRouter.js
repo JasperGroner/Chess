4d77bd8939602792319caac5d618d0d7a2ece7ca
Jasper Groner <jasper.groner@gmail.com>
2023-02-14 17:40:54 -0500

    Make New Game storage in backend
    Only shows NewGameForm if you are logged in

--- server/src/routes/api/v1/gamesRouter.js
+++ server/src/routes/api/v1/gamesRouter.js
@@ -1,6 +1,7 @@
 import express from "express"
-import Game from "../../../models/Game.js"
+import { Game, Player, GameState } from "../../../models/index.js"
 import GameSerializer from "../../../serializers/GameSerializer.js"
+import cleanUserInput from "../../../services/cleanUserInput.js"
 
 const gamesRouter = new express.Router()
 
@@ -22,6 +23,21 @@ gamesRouter.get("/:gameId/load", async (req, res) => {
     const game = await Game.query().findById(gameId)
     const serializedGame = await GameSerializer.getDetail(game, userId)
     res.status(200).json({ game: serializedGame })
+  } catch(error) {
+    res.status(500).json({ errors: error })
+  }
+})
+
+gamesRouter.post("/hot-seat", async (req, res) => {
+  const userId = req.user.id
+  const { body } = req
+  body.gameType = "hot-seat"
+  const formPayload = cleanUserInput(body)
+  try {
+    const newGame = await Game.query().insertAndFetch(formPayload)
+    const newPlayer = await Player.query().insertAndFetch({userId, gameId: newGame.id})
+    const newGameState = await GameState.query().insertAndFetch({gameId: newGame.id, encodedState: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"})
+    res.status(200).json({game: newGame})
   } catch(error) {
     res.status(500).json({ errors: error })
   }
