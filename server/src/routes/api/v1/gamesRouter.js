ef1544ee309626a538980e56df47a25a56718b0e
Jasper Groner <jasper.groner@gmail.com>
2023-02-14 19:01:13 -0500

    implement auto-saving

--- server/src/routes/api/v1/gamesRouter.js
+++ server/src/routes/api/v1/gamesRouter.js
@@ -2,6 +2,8 @@ import express from "express"
 import { Game, Player, GameState } from "../../../models/index.js"
 import GameSerializer from "../../../serializers/GameSerializer.js"
 import cleanUserInput from "../../../services/cleanUserInput.js"
+import gamesGameStatesRouter from "./gamesGameStatesRouter.js"
+import { ValidationError } from "objection"
 
 const gamesRouter = new express.Router()
 
@@ -28,19 +30,24 @@ gamesRouter.get("/:gameId/load", async (req, res) => {
   }
 })
 
-gamesRouter.post("/hot-seat", async (req, res) => {
+gamesRouter.post("/hotSeat", async (req, res) => {
   const userId = req.user.id
   const { body } = req
-  body.gameType = "hot-seat"
+  body.gameType = "hot seat"
   const formPayload = cleanUserInput(body)
   try {
     const newGame = await Game.query().insertAndFetch(formPayload)
     const newPlayer = await Player.query().insertAndFetch({userId, gameId: newGame.id})
     const newGameState = await GameState.query().insertAndFetch({gameId: newGame.id, encodedState: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"})
-    res.status(200).json({game: newGame})
+    return res.status(200).json({game: newGame})
   } catch(error) {
-    res.status(500).json({ errors: error })
+    if (error instanceof ValidationError) {
+      return res.status(422).json({ errors: error.data })
+    }
+    return res.status(500).json({ errors: error })
   }
 })
+
+gamesRouter.use("/:gameId/gameState", gamesGameStatesRouter)
 
 export default gamesRouter
