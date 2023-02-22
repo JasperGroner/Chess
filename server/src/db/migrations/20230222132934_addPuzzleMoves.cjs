7dc96eb946229b2b64a028729701dfcce366c336
Jasper Groner <jasper.groner@gmail.com>
2023-02-22 08:34:04 -0500

    create new migration for puzzle moves

---
+++ server/src/db/migrations/20230222132934_addPuzzleMoves.cjs
@@ -0,0 +1,23 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("puzzleMoves", table => {
+    table.bigIncrements("id")
+    table.string("move").notNullable()
+    table.bigInteger("gameId").unsigned().notNullable().index().references("games.id").onDelete("CASCADE")
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+  })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists("puzzleMoves")
+}
